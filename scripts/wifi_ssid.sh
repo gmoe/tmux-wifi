cddfae3574fc36b0ee7239d049de2b907f913884
Oliver Weyhmueller <oliver@weyhmueller.de>
2017-04-01 12:11:57 +0200

    Show full SSID, even witth blanks
    * When SSID containes spaces, wifi_ssid.sh cuts the SSID at the first
      space. Changed awk expression to handle this correctly.

--- scripts/wifi_ssid.sh
+++ scripts/wifi_ssid.sh
@@ -5,7 +5,7 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 wifi_ssid() {
-  airport_status | grep -e '\bSSID:\B' | awk '{print $2}'
+  airport_status | grep -e '\bSSID:\B' | awk '{for(i=n;i<=NF;i++)$(i-(n-1))=$i;NF=NF-(n-1);print $0}' n=2
 }
 
 main() {
